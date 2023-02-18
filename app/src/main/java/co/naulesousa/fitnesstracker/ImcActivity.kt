e79f1033a10eb5c376d677c2660ea16dd2ee449e
Naule Sousa <davenstall@gmail.com>
2023-02-18 20:48:25 -0300

    validating form

--- app/src/main/java/co/naulesousa/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/naulesousa/fitnesstracker/ImcActivity.kt
@@ -2,10 +2,24 @@ package co.naulesousa.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeigth: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeigth = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            
+        }
     }
 }
