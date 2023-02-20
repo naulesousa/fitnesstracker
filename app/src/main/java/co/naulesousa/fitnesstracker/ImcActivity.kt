0939280b80882dbcb0d5a418885021534aa7bd6b
Naule Sousa <davenstall@gmail.com>
2023-02-20 19:42:40 -0300

    validating form (error message with toast)

--- app/src/main/java/co/naulesousa/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/naulesousa/fitnesstracker/ImcActivity.kt
@@ -4,32 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
 
-    private lateinit var editWeigth: EditText
+    private lateinit var editWeight: EditText
     private lateinit var editHeight: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
 
-        editWeigth = findViewById(R.id.edit_imc_weight)
+        editWeight = findViewById(R.id.edit_imc_weight)
         editHeight = findViewById(R.id.edit_imc_height)
 
         val btnSend: Button = findViewById(R.id.btn_imc_send)
         btnSend.setOnClickListener {
-
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
         }
     }
 
     private fun validate(): Boolean {
-        if (editWeigth.text.toString().isNotEmpty() && editHeight.text.toString().isNotEmpty()
-            && !editWeigth.text.toString().startsWith("0")
-            && !editHeight.text.toString().startsWith("0")) {
-            return true
-        } else {
-            return false
-        }
+        return (editWeight.text.toString().isNotEmpty() && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
     }
 }
