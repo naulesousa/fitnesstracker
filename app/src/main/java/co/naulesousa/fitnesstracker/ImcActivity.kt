ef4417a6bc9f4bc9560b384fd4ce5f3bf35bbe28
Naule Sousa <davenstall@gmail.com>
2023-02-22 19:15:45 -0300

    show result for user (imcResponse)

--- app/src/main/java/co/naulesousa/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/naulesousa/fitnesstracker/ImcActivity.kt
@@ -29,6 +29,29 @@ class ImcActivity : AppCompatActivity() {
             val height = editHeight.text.toString().toInt()
 
             val result = calculate(weight, height)
+
+            val imcResponseId = imcResponse(result)
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun imcResponse(imc: Double): Int {
+        if (imc < 15.0) {
+            return R.string.imc_severely_low_weight
+        } else if (imc < 16.0) {
+            return R.string.imc_very_low_weight
+        } else if (imc < 18.5) {
+            return R.string.imc_low_weight
+        } else if (imc < 25.0) {
+            return R.string.normal
+        } else if (imc < 30.0) {
+            return R.string.imc_high_weight
+        } else if (imc < 35.0) {
+            return R.string.imc_so_high_weight
+        } else if (imc < 40.0) {
+            return R.string.imc_severely_high_weight
+        } else {
+            return R.string.imc_extreme_weight
         }
     }
 
