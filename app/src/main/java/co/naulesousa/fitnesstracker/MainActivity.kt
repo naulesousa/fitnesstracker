3246afc4b7e16491fd0b1d8ec8e337d7dc68a8c9
Naule Sousa <davenstall@gmail.com>
2023-03-24 11:41:17 -0300

    dynamic data in recyclerview

--- app/src/main/java/co/naulesousa/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/naulesousa/fitnesstracker/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -25,6 +28,14 @@ class MainActivity : AppCompatActivity() {
                 drawableId = R.drawable.ic_baseline_wb_sunny_24,
                 textStringId = R.string.label_imc,
                 color = Color.GREEN,
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.ic_baseline_visibility_24,
+                textStringId = R.string.label_tmb,
+                color = Color.YELLOW,
             )
         )
 
@@ -59,7 +70,13 @@ class MainActivity : AppCompatActivity() {
 
     private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(item: MainItem) {
-            val buttonTest: Button = itemView.findViewById(R.id.btn_item)
+            val img: ImageView = itemView.findViewById(R.id.item_img_icon)
+            val name: TextView = itemView.findViewById(R.id.item_txt_name)
+            val container: LinearLayout = itemView as LinearLayout
+
+            img.setImageResource(item.drawableId)
+            name.setText(item.textStringId)
+            container.setBackgroundColor(item.color)
         }
     }
 }
